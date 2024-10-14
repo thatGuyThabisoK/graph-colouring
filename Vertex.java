c9d5d5f13fc545c40df506cc49852e19ea39fbdb
Thabiso kobe <47216749+thatGuyThabisoK@users.noreply.github.com>
2024-10-14 14:56:12 +0200

    Update Vertex.java
    we can now color each node

--- Vertex.java
+++ Vertex.java
@@ -1,20 +1,27 @@
 package src;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+
+import java.util.Map;
+
+
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
-public class Vertex {
+public class Vertex implements Comparator<Vertex>{
 	
-	private int colour,vertexNumber;
+	ArrayList<Color> myColors = new ArrayList<>();
+	
+	
+	private int colour,vertexNumber = -1111;
 	ArrayList<Vertex> adjacentList = new ArrayList<>();
 	private Shapes myShapeObj = new Shapes();
 	private Circle circleNode;
-	private HashMap<String, Line> links = new HashMap<>();	
+	HashMap<String, Line> links = new HashMap<>();	
 	
 	public Vertex(int vertexNumber) {
 		
@@ -23,15 +30,23 @@ public class Vertex {
 		circleNode = myShapeObj.createCircle(100,100,25);
 		myShapeObj.setOnDrag(circleNode,vertexNumber);
 		
+		myColors.add(Color.BEIGE);
+		myColors.add(Color.CRIMSON);
+		myColors.add(Color.PINK);
+		myColors.add(Color.BLACK);
+		
+		
 	}
 	
 	public void updateLink(double d, double e) {
 		for(Map.Entry<String, Line> entry : links.entrySet()) {
 			
 			// for each line in the vertex we make sure that we update the correct points
-			//by first checking 
+			//each key in the map is an edge of the form "3,2" ,each vertex will have a key of this form
+			// e.g vertex 3 will have an edge 3,2 in its links hash map and so will vertex 2
+			String[] startOrEnd = entry.getKey().split(",");
 			
-			if(entry.getKey().contains("start")) {
+			if(startOrEnd[0].equals(String.valueOf(this.vertexNumber))) {
 				entry.getValue().setStartX(d);
 				entry.getValue().setStartY(e);
 			}else {
@@ -53,12 +68,16 @@ public class Vertex {
 	
 	
 	
-	public boolean isAdjancent(Vertex v) {
+	public boolean isAdjancent(Vertex v){
 		
 		for(Vertex e : adjacentList) {
+			
 			if(e.vertexNumber == v.vertexNumber) {
+				
 				return true;
+				
 			}
+			
 		}
 		
 		return false;
@@ -69,48 +88,77 @@ public class Vertex {
 		
 		int[] used = new int[degree];
 		int index = 0;
+		
+	
+			
 		for(Vertex curr : adjacentList) {
 			if(curr.getColour() != -1) {
 				used[curr.getColour()] = 1;
 			}
-			
+				
 		}
-		
+			
 		for(int i = 0; i < degree; ++i) {
 			if(used[i] == 0) {
 				index = i;
 				break;
 			}
 		}
+			
+	 colour = index;
+			
+			
 		
-		colour = index;
+		
 		
 	}
 	
-	public void addLink(String startOrEnd,Line line) { 
+	public boolean addLink(String edges,Line line) { 
 	
-		if(!links.containsKey(startOrEnd)) {
-			
-			links.put(startOrEnd,line);
-			
-		}else {
-				
-			addLink(startOrEnd.concat(Integer.toString(new Random().nextInt())), line);
+		//this function adds the line that links this vertex to other vertices to its links hash map 
+		//the point of the edges string is for us to be able to update the position
+		// of the correct end of the line for instance if there is a node  at the end of a line, then when we
+		//move said node we know that we'll have to update the end of the line and we will keep the starting position the same
 		
+		if(!links.containsKey(edges)) {
+			
+			links.put(edges,line);
+			return true;
+			
 		}
+		
+		return false;
 		
 	}
 	
 	
 	public int getColour() { return colour;}
-	public void setC(int colour) {this.colour = colour;}
+	
 	
 	public int getVertexNumber() { return vertexNumber;}
+	public HashMap<String,Line> getLinks(){ return links;}
 	
 	public double getCenterX() { return circleNode.getCenterX();}
 	
 	public double getCenterY() { return circleNode.getCenterY();}
 	
 	public Circle myCircleNode() {return circleNode;}
+	public Shapes myShape() { return myShapeObj;}
 
+	public void changeColour(int dis) {
+		
+		circleNode.setFill(myColors.get(dis));
+		
+	}
+
+
+	@Override
+	public int compare(Vertex v1, Vertex v2) {
+		
+		return (v1.getDegree() > v2.getDegree())? 1 : 0;
+	}
+	
+
+
+	
 }
